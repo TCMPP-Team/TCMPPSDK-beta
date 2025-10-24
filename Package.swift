1026d6a028522e261dac5878abb4f1f96a19bfdf
stonelshi <stonelshi@tencent.com>
2025-10-24 16:50:53 +0800

    b5f11657786f721a80415adcb7f0ae9002f24366

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TCMPPSDK",
-            url: "https://tmf-warehouse-1314481471.cos.ap-beijing.myqcloud.com/tcmpp/ios-sdk/noshark/TCMPPSDK/2.2.10/34/TCMPPSDK_2.2.10.xcframework.zip",
-                        checksum: "a5501f3dbf6dc2f815b6893922158b24c8863426729913b0557f5b5572f05d8a"
+            url: "https://tmf-warehouse-1314481471.cos.ap-beijing.myqcloud.com/tcmpp/ios-sdk/noshark/TCMPPSDK/2.2.15.2/2/TCMPPSDK_2.2.15.2.xcframework.zip",
+                        checksum: "bdea6d9740cadb61306c3aa557aae93c8b4e2143f6976a6dd305ea99f0abad27"
         ),
         .target(
             name: "TCMPPSDKWrapper",
